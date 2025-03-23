4e2db54f7a506de3f2b07273e448ddfa4204587c
pnlt <pnlt170603@gmail.com>
2025-03-23 22:37:00 +0700

    feat(levelUnlockedLogic): PN

---
+++ Assets/_Project/Scripts/PN/LevelSelectionController.cs
@@ -0,0 +1,37 @@
+using Michsky.UI.Heat;
+using UnityEngine;
+
+public class LevelSelectionController : MonoBehaviour
+{
+    [SerializeField] private ChapterManager chapterManager;
+
+    private void Start() {
+        UpdateLevelStatus();
+    }
+
+    public void UpdateLevelStatus() {
+        for (int i = 0; i < chapterManager.chapters.Count; i++)
+        {
+            string chapterId = chapterManager.chapters[i].chapterID;
+
+            if (GameManager.Instance.IsLevelUnlocked(i))
+            {
+                if (i == GameManager.Instance.currentLevelIndex)
+                {
+                    ChapterManager.SetCurrent(chapterId);
+                }
+                else
+                {
+                    ChapterManager.SetUnlocked(chapterId);
+                }
+            }
+            else
+            {
+                ChapterManager.SetLocked(chapterId);
+            }
+        }
+
+        // Refresh UI
+        chapterManager.InitializeChapters();
+    }
+}
