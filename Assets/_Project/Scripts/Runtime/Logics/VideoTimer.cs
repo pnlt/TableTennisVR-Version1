db93ea038ec3fd1201a03775f69260f990f52bf8
pnlt <pnlt170603@gmail.com>
2025-03-26 23:34:20 +0700

    chore: cleanup

--- Assets/_Project/Scripts/Runtime/Logics/VideoTimer.cs
+++ Assets/_Project/Scripts/Runtime/Logics/VideoTimer.cs
@@ -5,32 +5,29 @@ using UnityEngine.Video;
 public class VideoTimer : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
-    
+
     private float timer = 0f;
     private double videoLength;
 
-    private void Awake()
-    {
+    private void Awake() {
         videoPlayer = GetComponent<VideoPlayer>();
     }
 
-    private void Start()
-    {
+    private void Start() {
         var totalFrame = videoPlayer.clip.frameCount;
         var frameRate = videoPlayer.clip.frameRate;
         videoLength = totalFrame / frameRate;
-        
+
         videoPlayer.Play();
     }
 
-    private void Update()
-    {
+    private void Update() {
         timer += Time.deltaTime;
 
         if (timer >= videoLength)
         {
             // TODO - transition back to the play scene   
-            SceneManager.LoadSceneAsync(2);
+            SceneManager.LoadSceneAsync(3);
         }
     }
 }
